d84f1255c0a10b835bd8a071fc0191fc47f06060
Jane <iamjane.gmail.com>
2017-06-24 19:47:17 -0700

    secretagents

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,12 +1,42 @@
+/# Create a method to take a secret message #/
+def encrypt(secret_message)
+  /# set the index and define the first letter of the message  #/
 
-list = 0
-def encrypt(get_string)
-  get_string = gets.chomp
-  list = get_string.index! +=1
+  index = 0
+  first_letter = secret_message[0]
+  /# Do something to each letter of the text #/
+
+  while index < secret_message.length - 1
+     /# figure out the index #/
+    p secret_message[index]
+         /# add one to each letter #/
+
+    secret_message[index] = secret_message[index + 1]
+             /# do that to each letter #/
+
+    index +=1
+  end
+               /# put the first letter back around #/
+
+  secret_message[-1] = first_letter
+  return secret_message
 
 end
 
-def decrypt
-  -= 1
+encrypt("mabel")
+
+def decrypt(secret_message)
+  index = 0
+  first_letter = secret_message[0]
+  while index < secret_message.length - 1
+    p secret_message[index]
+    secret_message[index] = secret_message[index - 1]
+    index +=1
+  end
+  secret_message[-1] = first_letter
+  return secret_message
+
 end
 
+encrypt("mabel")
+decrypt
