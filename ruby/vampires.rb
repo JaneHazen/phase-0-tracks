4cae5dcf4cb28d8295f497b080fec60f52eaf450
Jane <iamjane.gmail.com>
2017-06-24 19:25:13 -0700

    Vampires

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,5 +1,5 @@
 puts "How many employees will be processed in this survey?"
-employee_number = gets.chomp
+employee_number = gets.chomp.to_i
 
 def get_garlic_preference
   puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
@@ -24,53 +24,53 @@ def get_health_preference
   end
 
   if @health_preference == "y"
-      @health_preference = true
+    @health_preference = true
   elsif @health_preference == "n"
-      @health_preference = false
+    @health_preference = false
+  end
+end
+
+def get_allergies
+  @allergy_prompt =  "List your allergies one by one, hitting enter after each description. Type 'done' when you are finished."
+  puts @allergy_prompt
+  @allergies = gets.chomp
+  until @allergies == "sunshine" || @allergies == "done"
+    puts @allergy_prompt
+    @allergies = gets.chomp
   end
 end
 
 def get_age
   puts "How old are you?"
   @age = gets.chomp
-  until @age.is_an_int == true
-    puts "I need a number."
+  while @age.to_i == 0
+    puts "Invalid number. Type your age again."
     @age = gets.chomp
   end
 end
 
-def get_user_info
+def get_year
+  puts "What year were you born?"
+  @birth_year = gets.chomp
+  while @birth_year.to_i == 0
+    puts "Invalid number. Type your age again."
+    @birth_year = gets.chomp
+  end
+end
 
+def get_user_info
   puts "What is your name?"
   @name = gets.chomp
   get_age
-  puts "What year were you born?"
-  @birth_year = gets.chomp.to_i
+  get_year
+  @age = @age.to_i
+  @birth_year = @birth_year.to_i
   @real_age = 2017 - @birth_year
   get_garlic_preference
   get_health_preference
 end
 
-
-
-def get_allergies
-
-  @allergy_prompt =  "List your allergies one by one, hitting enter after each description. Type 'done' when you are finished."
-  puts @allergy_prompt
-  @allergies = gets.chomp.downcase
-  puts @allergies
-  while @allergies != "sunshine" && @allergies != "done"
-    puts @allergy_prompt
-    puts @allergies
-    @allergies = gets.chomp.downcase
-  end
-
-end
-
-
-
 def vampire_survey
-
   get_user_info
   get_allergies
   if @allergies == "sunshine"
@@ -113,7 +113,10 @@ def vampire_survey
   else
       get_allergies
   end
-
 end
 
+
 employee_number.times do vampire_survey end
+  puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
+
+
