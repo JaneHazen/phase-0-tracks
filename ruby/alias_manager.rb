9586035e5a2dbd66833a4189992e7d1100688235
Jane <iamjane.gmail.com>
2017-06-28 22:56:56 -0600

    Full project

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,5 +1,6 @@
   puts "Type a name to get a secret alias. Type done to exit"
   name = gets.chomp
+  hash_of_names = {}
 until name == "done"
 
   #reverse the order of the names
@@ -9,11 +10,22 @@ until name == "done"
   coded_name = reverse_name.tr("aeiouAEIOU", "uaeioUAEIO")
 
   puts "Your new alias is #{coded_name}."
-  hash_of_names = {name => coded_name}
+
+  # put it in a filler hash to merge later
+  filler_hash = {}
+  filler_hash.merge!(name => coded_name)
+
   hash_of_names[name] = coded_name
   puts "Would you like to add another name? Type 'done' to exit"
   name = gets.chomp
 end
-puts "Thank you #{name}... I mean #{coded_name}"
-p hash_of_names
+  hash_of_names.merge!(filler_hash)
+  first_key = hash_of_names.keys[0]
+  first_value = hash_of_names.values[0]
+
+puts "Thank you, #{first_key}... I mean #{first_value}!"
+
+  hash_of_names.each do |name, coded_name|
+    puts "The alias of #{name} is #{coded_name}."
+  end
 
