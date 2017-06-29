d38d46cb926a84b84607f79e3c92e3d0c5ef2a23
Jane <iamjane.gmail.com>
2017-06-28 19:22:58 -0600

    Add problem solving methods

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -13,3 +13,21 @@ arr = [42, 89, 23, 1]
 
 p search_array(arr, 4)
 
+# make an array that lists fibonacci numbers while the input is less than number given
+
+def fibonacci(n)
+  array = [0,]
+  index = 0
+  while index < n
+     a = 0
+     b = 1
+    while b < n do
+       array << b
+      a,b = b,a+b
+        end
+  index +=1
+  return array
+  end
+end
+
+p fibonacci( 6 )
