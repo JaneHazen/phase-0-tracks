cd785af8e7ad4223a03fac64fd48e361a14ac316
Jane <iamjane.gmail.com>
2017-07-15 16:25:11 -0500

    Add driver code and limit number of guesses

--- ruby/solo_project_game/game_code.rb
+++ ruby/solo_project_game/game_code.rb
@@ -28,3 +28,28 @@ class String_Work
 
 end
 
+puts "Ready Player One: type out your secret sentence!"
+secret_sentence = gets.chomp
+puts "Alright scroll down... "
+30.times {puts " " }
+
+puts "Ready Player Two: This is what you're working with:"
+string = String_Work.new
+puts ""
+puts string.String_to_hyphens(secret_sentence)
+puts "Guess a letter, Player Two:"
+
+letter_guess = gets.chomp
+counter = 0
+while  string.Are_we_done_yet(string.Guess_in_sentence(secret_sentence,letter_guess)) != "WINNA!" && counter < 5
+  counter += 1
+  puts string.Guess_in_sentence(secret_sentence, letter_guess)
+  puts string.Are_we_done_yet(string.Guess_in_sentence(secret_sentence,letter_guess))
+  print "You guessed #{counter} times, idiot. FYI you get 5 guesses."
+  puts " "
+  letter_guess = gets.chomp
+end
+
+if string.Are_we_done_yet(string.Guess_in_sentence(secret_sentence,letter_guess)) != "WINNA!"
+  puts "This is too much. You're done. You lost."
+end
