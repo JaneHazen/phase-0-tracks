4f93f863f234b83c1caac79487dc6921a34550a4
Jane <iamjane.gmail.com>
2017-07-15 15:26:40 -0500

    Add method that changes guesses to letters in string, RSpec Approved#100

--- ruby/solo_project_game/game_code.rb
+++ ruby/solo_project_game/game_code.rb
@@ -3,4 +3,19 @@ class String_Work
   def String_to_hyphens(sentence_goals)
     sentence_goals.gsub(/[a-zA-Z]/,"-")
   end
+
+  def Guess_in_sentence(sentence_goals, guess)
+    sentence_array = sentence_goals.split("")
+    sentence_array.map! do |x|
+      if x == guess
+        guess
+      elsif x == " "
+        " "
+      else
+        "-"
+      end
+    end
+    p sentence_array.join
+  end
+
 end
--- ruby/solo_project_game/game_code_spec.rb
+++ ruby/solo_project_game/game_code_spec.rb
@@ -1,9 +1,15 @@
 require_relative 'game_code'
 
 describe String_Work do
+
   it "changes a string into all hypens" do
       string = String_Work.new
-      expect string.String_to_hyphens("Jane").to eq ----
-    end
+      expect(string.String_to_hyphens("Jane")).to eq "----"
   end
+
+  it "iterates sentence and changes guess with the rest printing as hyphens" do
+      string = String_Work.new
+      expect(string.Guess_in_sentence("aaa","a")).to eq "aaa"
+  end
+
 end
