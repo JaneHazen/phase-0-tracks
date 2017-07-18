1be8203a6aad540d7c46ddb1e8a62c6da78fc5ca
Jane <iamjane.gmail.com>
2017-07-18 12:10:37 -0500

    Add pseudocode for reversing a string in js

---
+++ js/explore.js
@@ -0,0 +1,3 @@
+// Change a string into an array of letters. (.split?)
+// Iterate through the array backwards using the negative of the length of the word as a starting point and printing each letter.
+
