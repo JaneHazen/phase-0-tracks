a964e76cddc5cc06649fc35f4498adacf22e473d
Jane <iamjane.gmail.com>
2017-07-20 17:00:23 -0500

    Add release 1

--- js/algos.js
+++ js/algos.js
@@ -1,3 +1,5 @@
+// release 0
+//
 // create a function that will take an array as a parameter
 // iterate through each item on the array with a for loop
 // if the length of item one is greater than the next, return item (similar to the bubble-sort algorithm? but more clumsy because i have to do a comparison with the last one tacked on at the end)
@@ -17,14 +19,29 @@ function sizeMatters(array){
 
 console.log(sizeMatters(["jane","andy","ano","makaela"]))
 
+//release 1
 //
-//
-// We have to iterate through one hash and take each key and see if it corresponds to any of the keys in another hash (so iterate through that one)
+// We have to iterate through one hash and take each key and see if it corresponds to any of the keys in another hash
 // if yes, return the key+values
+
 function yourFunction(hash1, hash2){
-  for (var i = 0; i < hash1.length; i++){
-    if hash1.i == hash2[1]
-      return hash1, hash2;
+  var are_equal = false;
+  for (var prop in hash1){
+    if (hash1[prop] == hash2[prop]){
+      are_equal = true;
+    }
+    // console.log(`hash1.${prop} = ${hash1[prop]}`);
   }
+  console.log(are_equal);
 }
 
+
+yourFunction({name: "Steven", age: 54}, {name: "Tamir", age: 54});
+yourFunction({animal: "Dog", legs: 4}, {animal: "Cat", legs: 3});
+
+
+// release 2
+//
+// build an array of the alphabet
+//set a variable that will pick a random number and select that item from the alphabet array and print it out (random number 1-10) times
+// print those variables user_input times and put them in an array
